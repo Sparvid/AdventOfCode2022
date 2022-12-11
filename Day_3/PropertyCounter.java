971a07c37e34ad083a0c3aa38588b3c0930657b7
Sparvid <arvid@loren.se>
2022-12-11 13:32:36 +0100

    Day 3 part 1 and 2 done

--- Day_3/PropertyCounter.java
+++ Day_3/PropertyCounter.java
@@ -1,32 +1,37 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 
 
 public class PropertyCounter {
 
-    
-
     public static void main(String[] args) {
 
         String filename = "rucksacks.input";
+        int prioritySum = 0;
 
         try (Scanner sc = new Scanner(new File(filename))) {
 
             while (sc.hasNext()) {
-                String character = sc.next(); // för en charactär
-                String line = sc.nextLine(); // för en hel rad
+                String items1 = sc.nextLine(); // för en hel rad
+                String items2 = sc.nextLine();
+                String items3 = sc.nextLine();
+                Rucksack rucksack1 = new Rucksack(items1);
+                Rucksack rucksack2 = new Rucksack(items2);
+                Rucksack rucksack3 = new Rucksack(items3);
+                threeRucksacks rucksacks = new threeRucksacks(rucksack1, rucksack2, rucksack3);
 
-                int c = Integer.parseInt(line); // parsa en int
+                int priority = rucksacks.itemToPriority(rucksacks.badgeItem());
+                prioritySum += priority;
+
                 
             }
-        // System.out.println(result);
+        System.out.println(prioritySum);
 
         } catch (FileNotFoundException e) {
             System.err.println(filename + " does not exist!");
         }
     }
 
-    
 }
